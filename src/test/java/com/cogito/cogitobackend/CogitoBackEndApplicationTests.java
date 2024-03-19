46540ca405e174a0a6185ff00ce10a924b0fa55e
hilalgndgdu <you@example.com>
2024-03-19 17:16:34 +0300

    first commit

---
+++ src/test/java/com/cogito/cogitobackend/CogitoBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cogito.cogitobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CogitoBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
